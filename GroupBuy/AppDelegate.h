cc8918caa2171797681e940deba56c0fea8d8dc1
zhanglu1988em <zhanglu1988em163.com@zhanglu1988emdemacbook-pro.local>
2016-01-06 20:24:30 +0800

    Initial Commit

---
+++ GroupBuy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GroupBuy
+//
+//  Created by zhanglu1988em on 16/1/6.
+//  Copyright © 2016年 wbder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
